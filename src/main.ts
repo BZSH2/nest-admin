999a2ad6c1a8fe7209fb40788024cbe298a277ba
wangbo <99548050@qq.com>
2026-03-09 15:44:12 +0800

    chore: :rocket: lint相关配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  // API Prefix
+  const prefix = configService.get<string>('API_PREFIX') || 'api';
+  app.setGlobalPrefix(prefix);
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Global Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Global Filters
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Global Interceptors
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // CORS
+  app.enableCors();
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Nest Admin API')
+    .setDescription('The Nest Admin API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const swaggerPath = configService.get<string>('SWAGGER_PATH') || 'docs';
+  SwaggerModule.setup(swaggerPath, app, document);
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/${prefix}`);
+  console.log(`Swagger documentation is available at: http://localhost:${port}/${swaggerPath}`);
 }
 bootstrap();
